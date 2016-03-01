c4c8be590ea6d8ef942fac1fbeb0abc12200eb3a
DottyStyle <mark@thehallway.com.au>
2016-03-01 08:10:17 +0800

    changes

--- src/ios/PRAppLaunchKit.h
+++ src/ios/PRAppLaunchKit.h
@@ -12,8 +12,26 @@
 
 @interface PRAppLaunchKit : CDVPlugin
 
+@property (nonatomic, copy) NSString * subscriptionKey; // key used to access https://developers.pressreader.com
+
+@property (nonatomic, copy) NSString * scheme; // default 'PressReader'
+@property (nonatomic, copy) NSString * hostName; // default 'PressDisplay.com'
+@property (nonatomic, copy) NSString * appStoreID; // default '313904711'
 
 - (void)launchPR:(CDVInvokedUrlCommand*)command;
 
-@property (nonatomic, retain) IBOutlet UISwitch * removeAllContentSwitch;
++ (instancetype) defaultAppLaunch;
+
+- (instancetype) initWithScheme:(NSString *)scheme hostName:(NSString *)hostName appStoreID:(NSString *)appStoreID;
+- (instancetype) initWithScheme:(NSString *)scheme;
+
+- (BOOL) isAppInstalled;
+- (void) launchAppWithCommand:(NSString *)command URLParameters:(NSDictionary *)urlParameters;
+- (void) launchAppWithURLParameters:(NSDictionary *)urlParameters;
+- (void) installApp;
+
 @end
+
+
+
+
--- src/ios/PRAppLaunchKit.m
+++ src/ios/PRAppLaunchKit.m
@@ -16,4 +16,78 @@
 {
  
 }
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    self.durationTextField.enabled = NO;
+    self.durationTextField.text = @"1";
+    self.expirationCountdownLabel.hidden = YES;
+
+    //[PRAppLaunchKit defaultAppLaunch].subscriptionKey = @"d9d261747f4148aaad4d13b670a24129"; // please get one from api.pressreader.com assigned to your account
+    [self.launchButton setTitle:([[PRAppLaunchKit defaultAppLaunch] isAppInstalled] ? @"Open" : @"Install") forState:UIControlStateNormal];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+- (IBAction)onStepperChanged:(UIStepper *)stepper
+{
+    self.durationTextField.text = [NSString stringWithFormat:@"%.0f", stepper.value];
+}
+
+- (IBAction)launchPR:(id)sender {
+    NSDictionary * args = nil;
+    if (self.giftIDTextField.text.length && self.secretTextField.text.length) {
+        NSInteger siteID = self.siteIDTextField.text.integerValue ?: 8898;
+        NSString *secret = self.secretTextField.text;
+        NSString *giftID = self.giftIDTextField.text;
+        NSInteger duration = [self.durationTextField.text integerValue];
+        NSString *token = [self giftedJwtWithId:giftID siteID:siteID duration:duration tokenExpiration:24 signingSecret:secret];
+
+        args = @{@"jwt": token};
+    }
+
+     [[PRAppLaunchKit defaultAppLaunch] launchAppWithCommand:@"register-gifted-access" URLParameters:args];
+}
+
+#pragma mark - Creating JWT utility methods
+
+- (NSData *)HS256SignPayload:(NSString *)theString withSecret:(NSString *)theSecret;
+{
+    const char *cString = [theString cStringUsingEncoding:NSUTF8StringEncoding];
+    const char *cSecret = [theSecret cStringUsingEncoding:NSUTF8StringEncoding];
+
+    unsigned char cHMAC[CC_SHA256_DIGEST_LENGTH];
+    CCHmac(kCCHmacAlgSHA256, cSecret, strlen(cSecret), cString, strlen(cString), cHMAC);
+    return [NSData dataWithBytes:cHMAC length:sizeof(cHMAC)];
+}
+
+- (NSString *)giftedJwtWithId:(NSString *)issID siteID:(NSInteger)siteid duration:(NSInteger)accessH tokenExpiration:(NSInteger)tokenH signingSecret:(NSString *)secret
+{
+    NSString * header = @"{\"alg\": \"HS256\",\"typ\": \"JWT\"}";
+    long now = lround([[NSDate date] timeIntervalSince1970]);
+    NSDictionary * payload = @{@"iss":@"Qantas", @"jti":issID, @"iat":@(now), @"nbf":@(now-3600), @"exp":@(now + 3600 * tokenH), @"site-id":@(siteid), @"gift-period": @{@"num-hours":@(accessH)}};
+    NSString *b64Header = [[[header dataUsingEncoding:NSUTF8StringEncoding] base64EncodedStringWithOptions:0] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"="]];
+    NSString *b64payload = [[[NSJSONSerialization dataWithJSONObject:payload options:0 error:NULL] base64EncodedStringWithOptions:0] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"="]];
+    NSString *signingInput = [@[b64Header, b64payload] componentsJoinedByString:@"."];
+    NSString *signingOutput = [[[self HS256SignPayload:signingInput withSecret:secret] base64EncodedStringWithOptions:0] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"="]];
+
+    return [@[signingInput, signingOutput] componentsJoinedByString:@"."];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+ 
 @end
