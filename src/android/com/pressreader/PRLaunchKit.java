516024da0de1bfee31692929d1363757dba872e9
DottyStyle <mark@thehallway.com.au>
2016-04-14 10:20:46 +0800

    Added version for android

---
+++ src/android/com/pressreader/PRLaunchKit.java
@@ -0,0 +1,30 @@
+package com.pressreader;
+
+import android.app.Activity;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Context;
+
+public class PRLaunchKit extends CordovaPlugin {
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("launchPressReader")) {
+      Context context = this.cordova.getActivity().getApplicationContext();
+
+      JSONObject result = new JSONObject();
+    
+      result.put("mnc", 1);
+
+      callbackContext.success(result);
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
