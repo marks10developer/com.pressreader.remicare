b4eea9e4a44d9280bb8560c40fc78f271aa55be4
DottyStyle <mark@thehallway.com.au>
2016-04-14 10:47:46 +0800

    Logging in android

--- src/android/com/pressreader/PRLaunchKit.java
+++ src/android/com/pressreader/PRLaunchKit.java
@@ -1,6 +1,7 @@
 package com.pressreader;
 
 import android.app.Activity;
+import android.util.Log;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONObject;
@@ -17,9 +18,9 @@ public class PRLaunchKit extends CordovaPlugin {
       Context context = this.cordova.getActivity().getApplicationContext();
 
       JSONObject result = new JSONObject();
-    
+      
       result.put("mnc", 1);
- 
+      Log.d("TAG", "Message");
       callbackContext.success(result);
 
       return true;
