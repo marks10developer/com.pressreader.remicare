62241358f679e2606b9a277dd8a135732988f61b
mark@dottystylecreative.com <mark@dottystylecreative.com>
2016-04-15 12:12:05 +0800

    changed context

--- src/android/com/pressreader/PRLaunchKit.java
+++ src/android/com/pressreader/PRLaunchKit.java
@@ -17,18 +17,12 @@ public class PRLaunchKit extends CordovaPlugin {
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     if (action.equals("launchPressReader")) {
-      Context context = this.cordova.getActivity().getApplicationContext();
-
+      Context context = this.cordova.getActivity();
       JSONObject result = new JSONObject();
-
-      //result.put("mnc", 1);
-      //Log.d("TAG", "Message");
-
-      System.out.println("TEST");
       pressReaderLaunchHelper = new PressReaderLaunchHelper("d9d261747f4148aaad4d13b670a24129");
       pressReaderLaunchHelper.launchPressReaderWithGiftAccess(context, "secret_welcome1", "8898", "secret_welcome1", 24);
+      System.out.println("PRESSREADER Run");
       callbackContext.success(result);
-
       return true;
     } else {
       return false;
